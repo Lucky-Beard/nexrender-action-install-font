a1cad6d9ea2cafafff9a41f6261692294c8039fd
Morne <morne@luckybeard.com>
2020-11-06 09:24:16 +0200

    More testing

--- index.js
+++ index.js
@@ -5,32 +5,23 @@ const {
 const fs = require('fs');
 
 module.exports = (job, settings, action, type) => {
-  const WORKING_DIR = settings.workpath + '/' + job.uid;
-  const FONT_DIR = '~/Documents/fakeFontDir/';
-  // const FONT_DIR = 'C:\\windows\\fonts\\';
+  const WORKING_DIR = 'd:\\Lucky Render Worker\\temp\\' + job.uid;
+  const FONT_DIR = 'c:\\Windows\\fonts\\';
   const { fonts } = action;
   if (!fonts.length) {
     return "Make sure to specify a font as a string array e.g. ['FontName.ttf']";
   }
 
   let promises = fonts.map(async (font) => {
-    // check if font already exists in location
-
-    // return fs.existsSync(WORKING_DIR + '/' + font);
-
-    console.log(' FILE EXISTS< LET?S COPY');
-    return fsPromises
-      .copyFile(WORKING_DIR + '/' + font, '~/Documents/fakeFontDir/test.ttf')
-      .then(() => console.log(`${font} was copied and installed`))
-      .catch((err) => console.log(err, 'The file could not be copied'));
-    // if (!fs.existsSync(FONT_DIR + font)) {
-    //   return fsPromises
-    //     .copyFile(WORKING_DIR + '/' + font, '~/Documents/fakeFontDir/test.ttf')
-    //     .then(() => console.log(`${font} was copied and installed`))
-    //     .catch((err) => console.log(err, 'The file could not be copied'));
-    // } else {
-    //   return 'Font already exists';
-    // }
+    // if it doesn't exist, copy it in
+    if (!fs.existsSync(FONT_DIR + font)) {
+      return fsPromises
+        .copyFile(WORKING_DIR + '\\' + font, FONT_DIR + font)
+        .then(() => console.log(`${font} was copied and installed`))
+        .catch((err) => console.log(err, 'The file could not be copied'));
+    } else {
+      return;
+    }
   });
 
   return Promise.all(promises);
