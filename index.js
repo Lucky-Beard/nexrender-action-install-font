9b8341a3e6479b4461b3605c092fbf311724b0ef
Morne <morne@luckybeard.com>
2020-11-06 17:33:38 +0200

    Version 1.0.4 changes on error handling

--- index.js
+++ index.js
@@ -1,51 +1,83 @@
 const {
   promises: fsPromises,
-  constants: { COPYFILE_EXCL },
 } = require('fs');
 const fs = require('fs');
-const { exec, execSync } = require('child_process');
+const { execSync } = require('child_process');
+const FONT_REG =
+  'HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts';
+const FONT_DIR = 'c:\\Windows\\Fonts\\';
+
 
 module.exports = (job, settings, { fonts, workingDirectory }, type) => {
-  const WORKING_DIR = workingDirectory + job.uid;
-  const FONT_REG =
-    'HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts';
-  const FONT_DIR = 'c:\\Windows\\fonts\\';
+  const WORKING_DIR = `${workingDirectory}/${job.uid}`;
+
   if (!fonts.length) {
     return "Make sure to specify a font as a string array e.g. ['FontName.ttf']";
   }
 
   let promises = fonts.map(async (font) => {
-    const extensionPos = font.indexOf('.');
 
+    const extensionPos = font.indexOf('.');
     if (extensionPos < 0) {
       return 'Incorrect font name supplied, should be name plus extension e.g. Arial.ttf';
     }
     const fontName = font.substr(0, extensionPos);
 
-    try {
-      execSync(`REG QUERY "${FONT_REG}" /v "${name}"`);
-      console.log('Already Installed');
-      return 'Already Installed';
-    } catch (err) {
-      // if it doesn't exist, copy it in
-      if (!fs.existsSync(FONT_DIR + font)) {
-        try {
-          execSync(
-            `REG ADD "${FONT_REG}" /v "${fontName}" /t REG_SZ /d ${font} /f`
-          );
-          return fsPromises
-            .copyFile(WORKING_DIR + '\\' + font, FONT_DIR + font)
-            .then(() => console.log(`${font} was copied and installed`))
-            .catch((err) => console.log(err, 'The file could not be copied'));
-        } catch (err) {
-          throw new Error(err);
-        }
-      } else {
-        console.log('Already Installed');
-        return 'Already Installed';
-      }
+    const registryExists = await registryEntryExists(fontName);
+
+    if (registryExists) {
+      return copyFile(WORKING_DIR + '\\' + font, FONT_DIR + font);
+    } else {
+      return installPromises = await Promise.all([addToRegistry(font, fontName), copyFile(WORKING_DIR + '\\' + font, FONT_DIR + font)])
     }
+
   });
 
-  return Promise.all(promises);
+  return Promise.all(promises).then(res => {
+    console.log("SUCCESSFULLY INSTALLED FONT", res);
+    return Promise.resolve(job);
+  }).catch(err => {
+    console.log("Failed to install font", err);
+    return Promise.reject(err);
+  })
 };
+
+fileExists = (path) => {
+  return fs.existsSync(path)
+}
+
+addToRegistry = async (font, fontName) => {
+  try {
+    execSync(
+      `REG ADD "${FONT_REG}" /v "${fontName} (TrueType)" /t REG_SZ /d "${font}" /f`
+    );
+    console.log("Font was successfully added to registry")
+    return 'Font was successfully added to registry'
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+copyFile = async (source, destination) => {
+  if (!fileExists(destination)) {
+    return fsPromises
+      .copyFile(source, destination).then(res => {
+        console.log("Successfully copied font");
+      }).catch((err) => {
+        console.log("Copying error ocurred: ", err);
+      });
+  } else {
+    console.log("Font already exists");
+    return 'Font already exists'
+  }
+
+}
+
+registryEntryExists = async (fontName) => {
+  try {
+    execSync(`REG QUERY "${FONT_REG}" /v "${fontName} (TrueType)"`);
+    return true
+  } catch (error) {
+    return false
+  }
+}
